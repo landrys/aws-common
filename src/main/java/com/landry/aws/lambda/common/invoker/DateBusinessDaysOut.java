f65c665493effec50e04101f14e5b65658dcbee8
Leon Landry <landryseleven@gmail.com>
2017-06-01 12:07:04 -0400

    Clean up

--- src/main/java/com/landry/aws/lambda/common/invoker/DateBusinessDaysOut.java
+++ src/main/java/com/landry/aws/lambda/common/invoker/DateBusinessDaysOut.java
@@ -3,9 +3,10 @@ package com.landry.aws.lambda.common.invoker;
 import com.amazonaws.services.lambda.invoke.LambdaFunction;
 import com.landry.aws.lambda.common.model.BusinessDayInput;
 import com.landry.aws.lambda.common.model.BusinessDayOutput;
+import com.landry.aws.lambda.common.util.LambdaFunctions;
 
 public interface DateBusinessDaysOut
 {
-  @LambdaFunction(functionName="dateBusinessDaysOut")
+  @LambdaFunction(functionName=LambdaFunctions.DATE_BUSINESS_DAYS_OUT)
   BusinessDayOutput dateBusinessDaysOut(BusinessDayInput input);
 }
---
+++ src/main/java/com/landry/aws/lambda/common/util/LambdaFunctions.java
@@ -0,0 +1,11 @@
+package com.landry.aws.lambda.common.util;
+
+public interface LambdaFunctions
+{
+
+	public static final String DATE_BUSINESS_DAYS_OUT = "dateBusinessDaysOut";
+	public static final String DUE_DATE = "dueDate";
+	public static final String LC_VENDOR_ADAPTER = "lcVendorAdapter";
+	public static final String GET_VENDOR_SHIP_TIMES = "getVendorShipTimes";
+
+}
