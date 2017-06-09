5aea5c6f61504a546a43929cb59579bada9ea369
Leon Landry <landryseleven@gmail.com>
2017-06-09 10:57:51 -0400

    Changed Functions to point to PROD alias

--- src/main/java/com/landry/aws/lambda/common/util/LambdaFunctions.java
+++ src/main/java/com/landry/aws/lambda/common/util/LambdaFunctions.java
@@ -3,9 +3,9 @@ package com.landry.aws.lambda.common.util;
 public interface LambdaFunctions
 {
 
-	public static final String DATE_BUSINESS_DAYS_OUT = "dateBusinessDaysOut";
-	public static final String DUE_DATE = "dueDate";
-	public static final String LC_VENDOR_ADAPTER = "lcVendorAdapter";
-	public static final String GET_VENDOR_SHIP_TIMES = "getVendorShipTimes";
+	public static final String DATE_BUSINESS_DAYS_OUT = "dateBusinessDaysOut:PROD";
+	public static final String DUE_DATE = "dueDate:PROD";
+	public static final String LC_VENDOR_ADAPTER = "lcVendorAdapter:PROD";
+	public static final String GET_VENDOR_SHIP_TIMES = "getVendorShipTimes:PROD";
 
 }
